39c088c5c63e77766aedc7aec29b86c29b8f753e
kotikkir9 <kirillzhukau@outlook.com>
2023-06-10 02:28:16 +0200

    Client: - Worked on implementing the form for creating a new game server

--- client/src/components/Home/CreateGameModal.jsx
+++ client/src/components/Home/CreateGameModal.jsx
@@ -10,32 +10,58 @@ function CreateGameModal(props) {
         setshowPassword(e.target.checked);
     }
 
+    function submit(form) {
+        form.preventDefault();
+
+        const inputs = form.target.elements;
+        const inputValues = {};
+
+        for (let i = 0; i < inputs.length; i++) {
+            const input = inputs[i];
+
+            if (input.localName === "input") {
+                inputValues[input.name] = input.value;
+            }
+        }
+
+        console.log(inputValues);
+    }
+
     return (
         <Modal class={styles.modal} onClose={props.onClose}>
             <p class={styles.text}>Create New Game</p>
-            <section class={styles["form-container"]}>
-                <div class={styles.form}>
-                    <label>Server Name</label>
-                    <input type="text" class="w-100" />
+            <form class={styles["form-container"]} onSubmit={submit}>
+                <div class={styles["form-inputs"]}>
+                    <div class={styles.form}>
+                        <label>Server Name</label>
+                        <input type="text" required class="w-100" name="name" />
+                    </div>
+                    <div class={styles.form}>
+                        <label>Password</label>
+                        <input
+                            type={showPassword() ? "text" : "password"}
+                            class="w-100"
+                            name="password"
+                        />
+                        {/* <input type="checkbox" onInput={triggerShowPassword} /> */}
+                    </div>
+                    <div class={styles.form}>
+                        <label>Max allowed players (2-10)</label>
+                        <input
+                            type="number"
+                            value={10}
+                            step={1}
+                            min={2}
+                            max={10}
+                            name="maxPlayers"
+                            required
+                        />
+                    </div>
                 </div>
-                <div class={styles.form}>
-                    <label>Password</label>
-                    <input
-                        type={showPassword() ? "text" : "password"}
-                        class="w-100"
-                    />
-                    <input type="checkbox" onInput={triggerShowPassword} />
-                </div>
-                <div class={styles.form}>
-                    <label>Max allowed players (2-10)</label>
-                    <input type="number" value={10} step={1} min={2} max={10} />
-                </div>
-            </section>
-            <section class={styles["btn-container"]}>
                 <Button class={`${styles["create-btn"]} primary w-100`}>
                     Create
                 </Button>
-            </section>
+            </form>
         </Modal>
     );
 }
